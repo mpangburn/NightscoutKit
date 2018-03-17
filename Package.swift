d0292f232b35a21ab96e0871d7650a70e9387b82
Michael Pangburn <michaelpangburn@comcast.net>
2018-03-17 00:38:28 -0700

    Add Package.swift

---
+++ Package.swift
@@ -0,0 +1,15 @@
+// swift-tools-version:4.0
+
+//
+//  Package.swift
+//  NightscoutKit
+//
+//  Created by Michael Pangburn on 3/17/18.
+//  Copyright © 2018 Michael Pangburn. All rights reserved.
+//
+
+import PackageDescription
+
+let package = Package(
+    name: "NightscoutKit"
+)
