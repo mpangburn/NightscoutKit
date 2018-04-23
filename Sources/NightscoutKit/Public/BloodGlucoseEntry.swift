5b6425ce96c4e341b0083e0ffb912413ff0f9ab7
Michael Pangburn <michaelpangburn@comcast.net>
2018-04-22 21:48:28 -0700

    Add documentation for BloodGlucoseEntry

--- Sources/NightscoutKit/Public/BloodGlucoseEntry.swift
+++ Sources/NightscoutKit/Public/BloodGlucoseEntry.swift
@@ -6,6 +6,8 @@
 //  Copyright © 2018 Michael Pangburn. All rights reserved.
 //
 
+/// A type representing a blood glucose entry.
 public protocol BloodGlucoseEntry: TimelineValue, BloodGlucoseUnitConvertible {
+    /// The recorded blood glucose value.
     var glucoseValue: BloodGlucoseValue { get }
 }
