474b15f5b40a4e664653dfbb6b6996189a39774c
mpangburn <mjpangbu@calpoly.edu>
2020-03-15 17:27:39 -0700

    Specify supported platforms

--- Package.swift
+++ Package.swift
@@ -4,6 +4,9 @@ import PackageDescription
 
 let package = Package(
     name: "NightscoutKit",
+    platforms: [
+        .macOS(.v10_12), .iOS(.v12), .watchOS(.v5), .tvOS(.v11)
+    ],
     products: [
         .library(name: "NightscoutKit", targets: ["NightscoutKit"])
     ],
