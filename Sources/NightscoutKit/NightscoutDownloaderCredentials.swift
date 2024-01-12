298b45b0d0e9e549bb5056362107a128428bb761
William <34038280+mullweisser@users.noreply.github.com>
2024-01-12 10:05:12 +0100

    Add  NightscoutDownloaderCredentials.swift

--- Sources/NightscoutKit/NightscoutDownloaderCredentials.swift
+++ Sources/NightscoutKit/NightscoutDownloaderCredentials.swift
@@ -15,15 +15,19 @@ public struct NightscoutDownloaderCredentials: Hashable, Codable {
     /// The Nightscout site URL.
     public let url: URL
 
+    /// The Nightscout site API secret.
+    public let apiSecret: String
+
     /// Validates a Nightscout site URL.
     /// - Parameter url: The Nightscout site URL to validate.
     /// - Parameter completion: The completion handler for the result of the validation request.
     /// - Parameter result: The result of the validation. In the case of success, this result will contain a valid credentials instance.
     public static func validate(
         url: URL,
+        apiSecret: String,
         completion: @escaping (_ result: NightscoutResult<NightscoutDownloaderCredentials>) -> Void
     ) {
-        let credentials = NightscoutDownloaderCredentials(url: url)
+        let credentials = NightscoutDownloaderCredentials(url: url, apiSecret: apiSecret)
         let testDownloader = NightscoutDownloader(credentials: credentials)
         // Verify URL by fetching a single entry.
         testDownloader.fetchMostRecentEntries(count: 1) { result in
